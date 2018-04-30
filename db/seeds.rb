6c3b580b6eb37dd0559c05e21ba4f4217efa561a
Alison <lagerlof.alison@gmail.com>
2018-04-30 20:26:26 +0200

    v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+Movie.create!([{
+  title: 'Forrest Gump',
+  release_year: 1994 ,
+  director:'Robert Zemeckis'},
+{ title: 'Pulp Fiction',
+  release_year: 1994 ,
+  director:'Quentin Tarantino'},
+{ title: 'Goodfellas' ,
+  release_year: 1990 ,
+  director:'Martin Scorsese'},
+{ title: 'Cidade de Deus',
+  release_year: 2002 ,
+  director:'Fernando Meirelles Katia Lund'},
+{ title: 'Léon' ,
+  release_year: 1994 ,
+  director:'Luc Besson'
+  }])
